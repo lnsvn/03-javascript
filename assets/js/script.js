fb256436e942722cd1a3bbefd877c6262f77009b
lnsvn <lsullivan301@yahoo.com>
2023-02-13 22:43:39 -0500

    Adds comments to Javascript file

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,51 +4,67 @@ var generateBtn = document.querySelector("#generate");
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// Arrays
+// arrays
 var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
 
-// Declarations
+// variable declarations
 var setLength = "";
 var addLower;
 var addUpper;
 var addNumber;
 var addSpecialChar;
 
-console.log(addLower);
-
-// Generate password criteria
+// generates password with criteria
 function generatePassword(){
+
+  //prompt for length
   var setLength = prompt("How many characters long would you like your password to be? Pick a number between 8-128.");
 
+  // checks with password length parameters
+  // loop
   while(setLength <= 7 || setLength >= 129){
+
+    // adds alert that selection was not in parameters
     alert("Password length must be 8-128 characters. Try again.");
+    // re-prompt
     var setLength = prompt("How many characters long would you like your password to be? Pick a number between 8-128.");
   }
 
+  // adds alert to inform of selection
   alert("Your password will contain " + setLength + " characters.");
 
+  // adds alert to give instruction
   alert("Next you will be prompted to add lowercase letters, uppercase letters, numbers, or special characters to your password. You can choose as many as you want, but you must add at least one.");
 
+  // prompts to confirm or deny criteria
   var addLower = confirm("Click OK to include lowercase letters in your password.");
   var addUpper = confirm("Click OK to include uppercase letters in your password.");  
   var addNumber = confirm("Click OK to include numbers in your password.");  
   var addSpecialChar = confirm("Click OK to include special characters in your password.");  
 
+  // checks with password character types parameters
+  // loop
   while(addLower === false && addUpper === false && addNumber === false && addSpecialChar === false){
+
+    // adds alert that selections were not in parameters
     alert("Password must contain at least one parameter. Try again.");
+
+    // re-prompts to confirm or deny
     var addLower = confirm("Click OK to include lowercase letters in your password.");
     var addUpper = confirm("Click OK to include uppercase letters in your password.");  
     var addNumber = confirm("Click OK to include numbers in your password.");  
     var addSpecialChar = confirm("Click OK to include special characters in your password.");  
   }
 
+  // adds alert to inform of selections
   alert("Your password will contain " + setLength + " characters. You selected the following parameters to be included - lowercase letters: " + addLower + ", uppercase letters: " + addUpper + ", numbers: " + addNumber + ", special characters: " + addSpecialChar + ".");
 
   var passChar = [];
 
+  // applies selections
   if (addLower){
   passChar = passChar.concat(lower);
   }
@@ -64,6 +80,7 @@ function generatePassword(){
 
   console.log(passChar);
 
+  // creates empty string that loop below will fill
   var randomPass = "";
 
   for (var i=0; i < setLength; i++){
